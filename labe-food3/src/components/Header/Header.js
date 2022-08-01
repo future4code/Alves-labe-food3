93e967d9b3d06f913ef73506f88173d574e0296d
Melissa Fernanda Copetti <melissacopetti@hotmail.com>
2022-08-01 17:26:49 -0300

    feitas constants, globalStyled e início do Header

--- labe-food3/src/components/Header/Header.js
+++ labe-food3/src/components/Header/Header.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from "react";
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import { HeaderBar, TitleBar } from "./Styled";
 
-export default function Header() {
+export default function DenseAppBar() {
   return (
-    <div>Header</div>
-  )
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <HeaderBar color="white" variant="dense">
+          <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+          </IconButton>
+          <TitleBar variant="h5" color="prymaryColor" component="div">
+            iFuture
+          </TitleBar>
+        </HeaderBar>
+      </AppBar>
+    </Box>
+  );
 }
---
+++ labe-food3/src/constants/colors.js
@@ -0,0 +1,3 @@
+export const primaryColor = "#000000"
+export const secundaryColor = "#E8222E"
+export const neutralColor = "#B8B8B8"
