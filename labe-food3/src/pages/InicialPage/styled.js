9013e6a46c8fa554441ddf19c181a39d1903fbf8
JordBousquet <jordbousuqet@yahoo.com.br>
2022-08-01 13:12:39 -0300

    Tela Incial App

---
+++ labe-food3/src/pages/InicialPage/styled.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+
+export const  ContanierLogo = styled.div`
+        height:100vh;
+        padding: 0px;
+        background-color:#e8222e;
+        display:flex;
+        align-items: center;
+        justify-content:center;
+`
