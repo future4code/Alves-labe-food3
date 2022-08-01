7043869221b931d02ffa543bdf6c9f7577674ad3
Gabriel Dos Santos Silva <gabrieldos55570000@gmail.com>
2022-08-01 11:35:00 -0300

    funcionamentos de rotas ok

--- labe-food3/src/routes/Routes.js
+++ labe-food3/src/routes/Routes.js
@@ -1,13 +1,20 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Header from "../components/Header/Header";
+import Inicio from "../pages/InicialPage/Inicio";
+import Login from "../pages/LoginPage/Login";
+import Cadastro from "../pages/CadastroPage/Cadastro";
+import Home from "../pages/HomePage/Home";
+import Perfil from "../pages/PerfilPage/Perfil";
+import Erro from "../pages/ErroPage/Erro";
 
-export default function Routes() {
+export default function Router() {
   return (
     <div>
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route index element={<Inicial/>} />
+          <Route index element={<Inicio/>} />
           <Route path="/login" element={< Login/>} />
           <Route path="/cadastro" element={<Cadastro />} />
           <Route path="/home" element={<Home />} />
