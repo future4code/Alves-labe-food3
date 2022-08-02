a114529a91c29cc002c2ddd4eb139ce2592814bf
Carlos Araújo <carlosaaraujopc@gmail.com>
2022-08-01 17:02:35 -0700

    Estilização da página de cadastro do endereço

--- labe-food3/src/pages/EnderecoPage/Endereco.js
+++ labe-food3/src/pages/EnderecoPage/Endereco.js
@@ -1,11 +1,93 @@
-import React from 'react';
+import React from "react";
+
+import TextField from "@mui/material/TextField";
+import { Button } from "@mui/material";
+import Stack from "@mui/material/Stack";
+
+import { ScreenContainer, InputsContainer } from "./styled";
 
 const Endereco = () => {
-    return (
-        <div>
-            <h1>Endereço</h1>
-        </div>
-    );
-}
+  return (
+    <ScreenContainer>
+      <p>Meu endereço</p>
+
+      <form>
+        <InputsContainer>
+          <TextField
+            label="Logradouro"
+            placeholder="Rua / Av."
+            type="text"
+            margin={"dense"}
+            name={"username"}
+            fullWidth
+            required
+            autoFocus
+          />
+          <TextField
+            label="Número"
+            placeholder="Número"
+            type="number"
+            margin={"dense"}
+            name={"numero"}
+            fullWidth
+            required
+          />
+          <TextField
+            label="Complemento"
+            placeholder="Apto. / Bloco"
+            type="text"
+            margin={"dense"}
+            name={"complemento"}
+            fullWidth
+          />
+          <TextField
+            label="Bairro"
+            placeholder="Bairro"
+            type="text"
+            margin={"dense"}
+            name={"bairro"}
+            fullWidth
+            required
+          />
+          <TextField
+            label="Cidade"
+            placeholder="Cidade"
+            type="text"
+            margin={"dense"}
+            name={"cidade"}
+            fullWidth
+            required
+          />
+          <TextField
+            label="Estado"
+            placeholder="Estado"
+            type="text"
+            margin={"dense"}
+            name={"estado"}
+            fullWidth
+            required
+          />          
+        </InputsContainer>
+
+        <Stack direction="column" spacing={2}>
+          <Button
+            sx={{
+              height: 51,
+              boxShadow: 0,
+              color: "white",
+            }}
+            fullWidth
+            size={"large"}
+            variant={"contained"}
+            type={"submit"}
+            color={"primary"}
+          >
+            Salvar
+          </Button>
+        </Stack>
+      </form>
+    </ScreenContainer>
+  );
+};
 
 export default Endereco;
---
+++ labe-food3/src/pages/EnderecoPage/styled.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const ScreenContainer = styled.div`
+	width: 100%;
+	min-height: 90vh;
+
+    display: flex;
+	flex-direction: column;
+
+    justify-content: center;
+	align-items: center;
+`;
+
+export const InputsContainer = styled.div`
+	width: 80vw;
+	max-width: 450px;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    margin: 15px 0;
+`;
