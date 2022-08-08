6f7786f5e4728c34a8a8023513eccf06330f3f30
JordBousquet <jordbousuqet@yahoo.com.br>
2022-08-07 21:55:56 -0300

    Resolvendo Conflitos

--- labe-food3/src/pages/PerfilPage/Perfil.js
+++ labe-food3/src/pages/PerfilPage/Perfil.js
@@ -5,73 +5,36 @@ import {useNavigate} from 'react-router-dom'
 import { useStyles } from './styled'
 import EditIcon from '../../assets/editIcon.png'
 import { goToEditUsuario, goToEditEndereco } from '../../routes/coordinator'
-import { useStyles } from "./styled";
+import Footer from "../../components/Footer/Footer";
 import { BASE_URL } from '../../constants/urls'
 import axios from 'axios'
 
+export default function Perfil() {
 
-// import React,{ useState, useEffect } from 'react'
-// import { Typography, ListItem, ListItemSecondaryAction, Box, ListItemText, Grid, IconButton, List, Paper } from "@mui/material";
-// import {useNavigate} from 'react-router-dom'
-// import { useStyles } from './styled'
-// import EditIcon from '../../assets/editIcon.png'
-// import { goToEditUsuario, goToEditEndereco } from '../../routes/coordinator'
-// import Footer from "../../components/Footer/Footer";
-// import { BASE_URL } from '../../constants/urls'
-// import axios from 'axios'
+  const [users, setUsers] = useState({})
+  const [history, setHistory] = useState([])
+
+  const classes = useStyles();
+  const navigate = useNavigate()
 
 
-// export default function Perfil() {
+useEffect(() => {
+  getProfile()
+  OrdersHistory()
+},[])
 
-  useEffect(() => {
-    GetProfile()
-    GetOrderHistory()
-  }, [])
+const getProfile = () =>{
+  axios.get(`${BASE_URL}profile`,{
+    headers:{
+      auth:localStorage.getItem('token')
+    }
+  }).then((resp) =>{
+      setUsers(resp.data.user)
+  }).catch((err)=>{
+      alert('Erro na requisição, tente novamente')
+  })
 
-
-  const GetProfile = () => {
-    axios.get(`${BASE_URL}profile`, {
-      headers: {
-        auth: localStorage.getItem("token"),
-      },
-    }).then((resp) => {
-      setUser(resp.data.user)
-    }).catch((err) => {
-      console.log(err)
-
-//   // const classes = useStyles();
-//   // const navigate = useNavigate();
-
-// useEffect(() => {
-//   getProfile()
-//   OrdersHistory()
-// },[])
-
-
-// const getProfile = () =>{
-//   axios.get(`${BASE_URL}profile`,{
-//     headers:{
-//       auth:localStorage.getItem('token')
-//     }
-//   }).then((resp) =>{
-//       setUsers(resp.data.user)
-//   }).catch((err)=>{
-//       alert('Erro na requisição, tente novamente')
-//   })
-
-// }
-
-
-// const OrdersHistory = () =>{
-//   axios.get(`${BASE_URL}orders/history`,{
-//     headers:{
-//       auth:localStorage.getItem('token')
-//     }
-//   }).then((resp) =>{
-//     setHistory(resp.data.orders)
-//   }).catch((err) =>{
-//     console.log(err)
-//   })
+}
 
 const OrdersHistory = () =>{
   axios.get(`${BASE_URL}orders/history`,{
@@ -84,39 +47,9 @@ const OrdersHistory = () =>{
     console.log(err)
   })
 
+}
 
-// }
-
-
-// console.log(history)
-
-// const ListHistory = history && history.map((OrderHistory) =>{
-//   const date = new Date(OrderHistory.expiresAt).toLocaleDateString(
-//     "pt-br"
-//   );
-
-//     return (
-//       <Grid container className={classes.ordersInnerGrid}>
-//       <Grid
-//         item
-//         xs={12}
-//         className={classes.orderGridItem}
-//       >
-//         <Paper className={classes.withPadding}>
-//           <Typography className={classes.restaurantText}>
-//             {OrderHistory.restaurantName}
-//           </Typography>
-//           <Typography className={classes.dateText}>
-//             {date}
-//           </Typography>
-//           <Typography className={classes.totalPriceText}>
-//             SUBTOTAL: {OrderHistory.totalPrice.toFixed(2)}R$
-//           </Typography>
-//         </Paper>
-//       </Grid>
-//     </Grid>
-//     )
-// })
+console.log(history)
 
 const ListHistory = history && history.map((OrderHistory) =>{
   const date = new Date(OrderHistory.expiresAt).toLocaleDateString(
@@ -125,32 +58,29 @@ const ListHistory = history && history.map((OrderHistory) =>{
 
     return (
       <Grid container className={classes.ordersInnerGrid}>
-        <Grid
-
-          item
-          xs={12}
-          className={classes.orderGridItem}
-        >
-          <Paper className={classes.withPadding}>
-            <Typography className={classes.restaurantText}>
-              {order.restaurantName}
-            </Typography>
-            <Typography className={classes.dateText}>
-              {date}
-            </Typography>
-            <Typography className={classes.totalPriceText}>
-              SUBTOTAL R${order.totalPrice.toFixed(2)}
-            </Typography>
-          </Paper>
-        </Grid>
+      <Grid
+        item
+        xs={12}
+        className={classes.orderGridItem}
+      >
+        <Paper className={classes.withPadding}>
+          <Typography className={classes.restaurantText}>
+            {OrderHistory.restaurantName}
+          </Typography>
+          <Typography className={classes.dateText}>
+            {date}
+          </Typography>
+          <Typography className={classes.totalPriceText}>
+            SUBTOTAL: {OrderHistory.totalPrice.toFixed(2)}R$
+          </Typography>
+        </Paper>
       </Grid>
-
+    </Grid>
     )
-  })
+})
 
   return (
     <div>
-      <Header title = "Meu Perfil" />
       <List>
         <ListItem className={classes.userListItem}>
           <Box className={classes.listBox}>
@@ -178,81 +108,11 @@ const ListHistory = history && history.map((OrderHistory) =>{
           </ListItemSecondaryAction>
         </ListItem>
 
-//   return (
-//     <div>
-//       <List>
-//         <ListItem className={classes.userListItem}>
-//           <Box className={classes.listBox}>
-//             <ListItemText primary={users.name} />
-//             <ListItemText primary={users.email} />
-//             <ListItemText primary={users.cpf} />
-//           </Box>
-//           <ListItemSecondaryAction>
-//             <IconButton
-//               edge="end"
-//               aria-label="edit"
-//               onClick={() => goToEditUsuario(navigate)}
-//             >
-//               <img className={classes.imgEditIcon} src={EditIcon} alt="Edit" />
-//             </IconButton>
-//           </ListItemSecondaryAction>
-//         </ListItem>
-
-
-//         <ListItem>
-//           <Box>
-//             <Typography>Endereço cadastrado</Typography>
-
-//             <ListItemText primary={users.address} />
-//           </Box>
-//           <ListItemSecondaryAction>
-//             <IconButton
-//               edge="end"
-//               aria-label="edit"
-//               onClick={() => goToEditEndereco(navigate)}
-//             >
-//               <img className={classes.imgEditIcon} src={EditIcon} alt="Edit" />
-//             </IconButton>
-//           </ListItemSecondaryAction>
-//         </ListItem>
-//         <Grid item xs={12} className={classes.ordersOutterGrid}>
-//           <Box className={classes.withBorderBottom}>
-//             <Typography className={classes.historyText}>
-//               Histório de pedidos
-//             </Typography>
-//           </Box>
-//         </Grid>
-//         <Grid item xs={12} className={classes.orderGridItem}>
-//           <Typography inline="true" align="center">
-//            {order === null ? (
-//               "Você não realizou nenhum pedido"
-//             ) : (
-//               <Grid container className={classes.ordersInnerGrid}>
-//                 <Grid item xs={12} className={classes.orderGridItem}>
-//                   <Paper className={classes.withPadding}>
-//                     <Typography className={classes.restaurantText}>
-//                       Bullguer Vila Madalena
-//                     </Typography>
-//                     <Typography className={classes.dateText}>
-//                       23 outubro 2019
-//                     </Typography>
-//                     <Typography className={classes.totalPriceText}>
-//                       SUBTOTAL R$67,00
-//                     </Typography>
-//                   </Paper>
-//                 </Grid>
-//               </Grid>
-//             )}
-// 
-//             {history.length === 0? 'Você não realizou nenhum pedido' : ListHistory}
-//
-//           </Typography>
-//         </Grid>
-//       </List>
-//       <Footer/>
-//     </div>
-//   );
-// }
+        <ListItem>
+          <Box>
+            <Typography >
+              Endereço cadastrado
+            </Typography>
 
             <ListItemText
               primary={users.address}
@@ -288,5 +148,3 @@ const ListHistory = history && history.map((OrderHistory) =>{
     </div>
   )
 }
-
-export default Perfil
