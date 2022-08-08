ff477472f65c666c04e7de2e77f2104bbc0e15bf
Melissa Fernanda Copetti <melissacopetti@hotmail.com>
2022-08-07 22:55:47 -0300

    alterações na página de cadastro

--- labe-food3/src/pages/CadastroPage/Cadastro.js
+++ labe-food3/src/pages/CadastroPage/Cadastro.js
@@ -1,15 +1,40 @@
 import React from "react";
 import TextField from "@mui/material/TextField";
-import { Button } from "@mui/material";
 import Stack from "@mui/material/Stack";
-import { ScreenContainer, InputsContainer, LogoImage, ButtonGo } from "./styled";
+import {
+  ScreenContainer,
+  InputsContainer,
+  LogoImage,
+  ButtonGo,
+  ChevronLeftIconE,
+} from "./styled";
 import Logo from "../../assents/logo.svg";
-import { goEndereco } from "../../routes/coordinator";
+import { goEndereco, goBack } from "../../routes/coordinator";
 import { useNavigate } from "react-router-dom";
 import Header from "../../components/Header/Header";
+import Visibility from "@material-ui/icons/Visibility";
+import VisibilityOff from "@material-ui/icons/VisibilityOff";
+import IconButton from "@material-ui/core/IconButton";
+import InputAdornment from "@material-ui/core/InputAdornment";
 
 export default function Cadastro() {
   const navigate = useNavigate();
+  const [values, setValues] = React.useState({
+    password: "",
+    showPassword: false,
+  });
+
+  const handleClickShowPassword = () => {
+    setValues({ ...values, showPassword: !values.showPassword });
+  };
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
+  const handlePasswordChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
 
   const onSubmitCadastro = () => {
     goEndereco(navigate);
@@ -17,76 +42,107 @@ export default function Cadastro() {
 
   return (
     <div>
-    <Header title ='Cadastrar' back ='true'/>
-    <ScreenContainer>
+      <Header
+        title={
+          <ChevronLeftIconE
+            onClick={() => goBack(navigate)}
+            sx={{ fontSize: 36 }}
+          />
+        }
+      />
+      <ScreenContainer>
+        <LogoImage src={Logo} alt={"Logo Labe Food"} />
+        <p>
+          <b>Cadastrar</b>
+        </p>
 
-      <LogoImage src={Logo} alt={"Logo Labe Food"} />
-      <p><b>Cadastrar</b></p>
+        <form onSubmit={onSubmitCadastro}>
+          <InputsContainer>
+            <TextField
+              label="Nome"
+              placeholder="Nome e sobrenome"
+              type="text"
+              margin={"dense"}
+              name={"username"}
+              fullWidth
+              required
+              autoFocus
+            />
+            <TextField
+              label="E-mail"
+              placeholder="email@email.com"
+              type="email"
+              margin={"dense"}
+              name={"email"}
+              fullWidth
+              required
+            />
+            <TextField
+              label="CPF"
+              placeholder="000.000.000-00"
+              type="number"
+              margin={"dense"}
+              name={"number"}
+              fullWidth
+              required
+            />
+            <TextField
+              label="Senha"
+              placeholder="Mínimo 6 caracteres"
+              margin={"dense"}
+              name={"password"}
+              fullWidth
+              required
+              type={values.showPassword ? "text" : "password"}
+              onChange={handlePasswordChange("password")}
+              value={values.password}
+              InputProps={{
+              endAdornment: <InputAdornment position="end">
+                  <IconButton
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                  >
+                    {values.showPassword ? <Visibility /> : <VisibilityOff />}
+                  </IconButton>
+                </InputAdornment>}}/>
+              <TextField
+              label="Confirmar"
+              placeholder="Confirmar a senha anterior"
+              margin={"dense"}
+              name={"password"}
+              fullWidth
+              required
+              type={values.showPassword ? "text" : "password"}
+              onChange={handlePasswordChange("password")}
+              value={values.password}
+              InputProps={{
+                endAdornment: 
+                <InputAdornment position="end">
+                  <IconButton
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                  >
+                    {values.showPassword ? <Visibility /> : <VisibilityOff />}
+                  </IconButton>
+                </InputAdornment>
+              }}
+            />
+          </InputsContainer>
 
-      <form onSubmit={onSubmitCadastro}>
-        <InputsContainer>
-          <TextField
-            label="Nome"
-            placeholder="Nome e sobrenome"
-            type="text"
-            margin={"dense"}
-            name={"username"}
-            fullWidth
-            required
-            autoFocus
-          />
-          <TextField
-            label="E-mail"
-            placeholder="email@email.com"
-            type="email"
-            margin={"dense"}
-            name={"email"}
-            fullWidth
-            required
-          />
-          <TextField
-            label="CPF"
-            placeholder="000.000.000-00"
-            type="number"
-            margin={"dense"}
-            name={"number"}
-            fullWidth
-            required
-          />
-          <TextField
-            label="Senha"
-            placeholder="Mínimo 6 caracteres"
-            type="password"
-            margin={"dense"}
-            name={"password"}
-            fullWidth
-            required
-          />
-          <TextField
-            label="Confirmar"
-            placeholder="Confirmar a senha anterior"
-            type="password"
-            margin={"dense"}
-            name={"password"}
-            fullWidth
-            required
-          />
-        </InputsContainer>
-
-        <Stack direction="column" spacing={2}>
-        <ButtonGo
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            margin="normal"
-            font-color="secondary"
-          >
-          <b>Criar</b>  
-          </ButtonGo>
-        </Stack>
-      </form>
-    </ScreenContainer>
+          <Stack direction="column" spacing={2}>
+            <ButtonGo
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              margin="normal"
+              font-color="secondary"
+            >
+              <b>Criar</b>
+            </ButtonGo>
+          </Stack>
+        </form>
+      </ScreenContainer>
     </div>
   );
 }
--- labe-food3/src/pages/HomePage/HomeTest.js
+++ labe-food3/src/pages/HomePage/HomeTest.js
@@ -1,199 +1,182 @@
-import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { gotoDetalhesPage } from '../../routes/coordinator'
-import { useEffect } from 'react'
-import { BASE_URL } from '../../constants/urls'
-import axios from 'axios'
+// import React, { useState } from 'react'
+// import { useNavigate } from 'react-router-dom'
+// import { gotoDetalhesPage } from '../../routes/coordinator'
+// import { useEffect } from 'react'
+// import { BASE_URL } from '../../constants/urls'
+// import axios from 'axios'
 
-export const HomeTest = () => {
-    const navigate = useNavigate()
-    const [restaurants, setRestaurants] = useState([])
-    const [foodType, setFoodType] = useState("");
-    const [food, setFood] = useState("")
+// export const HomeTest = () => {
+//     const navigate = useNavigate()
+//     const [restaurants, setRestaurants] = useState([])
+//     const [foodType, setFoodType] = useState("");
+//     const [food, setFood] = useState("")
 
-    const onClickCard = (id) => {
-   gotoDetalhesPage(navigate,id) }
+//     const onClickCard = (id) => {
+//    gotoDetalhesPage(navigate,id) }
 
 
-   useEffect(() => {
-        GetRestaurantes();
-      },[]);
+//    useEffect(() => {
+//         GetRestaurantes();
+//       },[]);
     
-      const GetRestaurantes = () => {
-        axios
-          .get(`${BASE_URL}/restaurants`, {
-            headers: {
-                Authorization: localStorage.getItem("token")
-            },
-          })
-          .then((resposta) => {
-           setRestaurants(resposta.data.restaurants)
-           console.log(resposta.data.restaurants)
-          })
-          .catch((erro) => {
-            alert(erro.response.data.message);
-          });
-      };
+//       const GetRestaurantes = () => {
+//         axios
+//           .get(`${BASE_URL}/restaurants`, {
+//             headers: {
+//                 Authorization: localStorage.getItem("token")
+//             },
+//           })
+//           .then((resposta) => {
+//            setRestaurants(resposta.data.restaurants)
+//            console.log(resposta.data.restaurants)
+//           })
+//           .catch((erro) => {
+//             alert(erro.response.data.message);
+//           });
+//       };
 
-  const onChangeFood = (value) => {
-    setFood(value);
-    switch (value) {
-      case "Árabe":
-        if (foodType === 1) {
-          setFoodType(0);
-        } else {
-          setFoodType(1);
-        }
-        break;
-      case "Asiática":
-        if (foodType === 2) {
-          setFoodType(0);
-        } else {
-          setFoodType(2);
-        }
-        break;
-      case "Hamburguer":
-        if (foodType === 3) {
-          setFoodType(0);
-        } else {
-          setFoodType(3);
-        }
-        break;
-      case "Italiana":
-        if (foodType === 4) {
-          setFoodType(0);
-        } else {
-          setFoodType(4);
-        }
-        break;
-      case "Sorvetes":
-        if (foodType === 5) {
-          setFoodType(0);
-        } else {
-          setFoodType(5);
-        }
-        break;
-      case "Carnes":
-        if (foodType === 6) {
-          setFoodType(0);
-        } else {
-          setFoodType(6);
-        }
-        break;
-      case "Baiana":
-        if (foodType === 7) {
-          setFoodType(0);
-        } else {
-          setFoodType(7);
-        }
-        break;
-      case "Petiscos":
-        if (foodType === 8) {
-          setFoodType(0);
-        } else {
-          setFoodType(8);
-        }
-        break;
-      case "Mexicana":
-        if (foodType === 9) {
-          setFoodType(0);
-        } else {
-          setFoodType(9);
-        }
-        break;
-      default:
-        setFoodType(0);
-    }
-  };
+//   const onChangeFood = (value) => {
+//     setFood(value);
+//     switch (value) {
+//       case "Árabe":
+//         if (foodType === 1) {
+//           setFoodType(0);
+//         } else {
+//           setFoodType(1);
+//         }
+//         break;
+//       case "Asiática":
+//         if (foodType === 2) {
+//           setFoodType(0);
+//         } else {
+//           setFoodType(2);
+//         }
+//         break;
+//       case "Hamburguer":
+//         if (foodType === 3) {
+//           setFoodType(0);
+//         } else {
+//           setFoodType(3);
+//         }
+//         break;
+//       case "Italiana":
+//         if (foodType === 4) {
+//           setFoodType(0);
+//         } else {
+//           setFoodType(4);
+//         }
+//         break;
+//       case "Sorvetes":
+//         if (foodType === 5) {
+//           setFoodType(0);
+//         } else {
+//           setFoodType(5);
+//         }
+//         break;
+//       case "Carnes":
+//         if (foodType === 6) {
+//           setFoodType(0);
+//         } else {
+//           setFoodType(6);
+//         }
+//         break;
+//       case "Baiana":
+//         if (foodType === 7) {
+//           setFoodType(0);
+//         } else {
+//           setFoodType(7);
+//         }
+//         break;
+//       case "Petiscos":
+//         if (foodType === 8) {
+//           setFoodType(0);
+//         } else {
+//           setFoodType(8);
+//         }
+//         break;
+//       case "Mexicana":
+//         if (foodType === 9) {
+//           setFoodType(0);
+//         } else {
+//           setFoodType(9);
+//         }
+//         break;
+//       default:
+//         setFoodType(0);
+//     }
+//   };
 
-const RestCards = restaurants.map((rest)=>{
-return (
-    <div
-    key={rest.id}
-    image={rest.logoUrl}
-    title={rest.name}
-    time={rest.deliveryTime}
-    price={rest.shipping}
-    onClick={()=> onClickCard(rest.id)}
-    />
-)
-})
+// const RestCards = restaurants.map((rest)=>{
+// return (
+//     <div
+//     key={rest.id}
+//     image={rest.logoUrl}
+//     title={rest.name}
+//     time={rest.deliveryTime}
+//     price={rest.shipping}
+//     onClick={()=> onClickCard(rest.id)}
+//     />
+// )
+// })
 
 
-const noneRestaurant = () => {
-        if (foodType === 0) {
-          const listRestaurants = restaurant
-            .filter((item) => {
-              return item.name
-                .tolowercase()
-                .includes(form.restaurant.toLowerCase());
-            })
-            .filter((restaurant) => {
-              return food === restaurant.category;
-            })
-            .map((restaurant) => {
-                <div
+// const noneRestaurant = () => {
+//         if (foodType === 0) {
+//           const listRestaurants = restaurants
+//             .filter((item) => {
+//               return item.name
+//                 .tolowercase()
+//                 .includes(form.restaurant.toLowerCase());
+//             })
+//             .filter((restaurant) => {
+//               return food === restaurant.category;
+//             })
+//             .map((restaurant) => {
+//                 <div
 //               key={restaurant.id}
 //               restaurant={restaurant}
 //               changePage={() => gotoDetalhesPage(restaurant.id)}
 //             />
-//           );
-//         });
-//       return listRestaurants;
-//     } else {
-//       const listRestaurants = restaurant
-//         .filter((item) => {
-//           return item.name
-//             .toLowerCase()
-//             .includes(form.restaurant.toLowerCase());
-//         })
-//         .filter((rest) => {
-//           return (
-//             <div
-//               key={rest.id}
-//               restaurant={rest}
-//               changePage={() => gotoDetalhesPage(rest.id)}
+          
+//         });} }
+//       return (noneRestaurant);
+ 
+
+
+
+
+
+
+//   return (
+//     <div>
+//             <Header title ='Ifuture'/>
+//         <main>
+//         <form>
+//          <SearchGroup>
+//            <ImputSearch
+//               name="restaurant"
+//               // value={form.restaurant}
+//               onChange={handleInputChange}
+//               type="search"
+//                id="outlined-basic"
+//               label="Restaurante"
+//                variant="outlined"
+//                required
+//                InputProps={{
+//                 startAdornment: (
+//                    <InputAdornment>
+//                      <IconButton>
+//                       <SearchIcon />
+//                     </IconButton>
+//                   </InputAdornment>
+//                 ),
+//               }}
 //             />
-//           );
-//         });
-//     }
-//   };
-
-
-
-
-
-
-  return (
-    <div>
-            <Header title ='Ifuture'/>
-        <main>
-        <form>
-         <SearchGroup>
-           <ImputSearch
-              name="restaurant"
-              // value={form.restaurant}
-              onChange={handleInputChange}
-              type="search"
-               id="outlined-basic"
-              label="Restaurante"
-               variant="outlined"
-               required
-               InputProps={{
-                startAdornment: (
-                   <InputAdornment>
-                     <IconButton>
-                      <SearchIcon />
-                    </IconButton>
-                  </InputAdornment>
-                ),
-              }}
-            />
-          </SearchGroup>
-         </form>
-         </main>
-        {RestCards}
+//           </SearchGroup>
+//          </form>
+//          </main>
+//         {RestCards}
     
-    </div>
-  )
-}
+//     </div>
+//   )
+// }
